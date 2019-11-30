8b54c42543de07169ebad6ffcb00222225af8216
Agus Mar <agustinam@gmail.com>
2019-11-29 22:04:12 -0300

    initial commit

---
+++ simonadice/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <img src="img/WWCode Logo_BUENOS AIRES-1.png" alt=""/>
+      </div>
+    );
+  }
+}
+
+export default App;
