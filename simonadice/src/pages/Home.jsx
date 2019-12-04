51480cefb3af4a24da10864c22517aaacb1f1fce
Agus Mar <agustinam@gmail.com>
2019-12-04 17:47:14 -0300

    agregué background image en home

--- simonadice/src/pages/Home.jsx
+++ simonadice/src/pages/Home.jsx
@@ -2,16 +2,18 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../components/images/Logo_WWCode_rainbow.png";
 import Start from "../components/Start";
-// import "./styles/Home.css";
+import "./styles/Home.css";
 
 class Home extends React.Component {
   render() {
     return (
       <section className="">
-        <img src={logo} alt=""/>
+        <div className="logo-container">
+          <img src={logo} alt="logo women who code" />
+        </div>
         <Link className="btn btn-primary" to="/game">
-          <Start />
-        </Link>
+            <Start />
+          </Link>
       </section>
     );
   }
