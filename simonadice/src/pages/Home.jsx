41703db9333178cbd7925bb792bc6d949e2a8481
Santiago <santiagoharkes@gmail.com>
2019-12-04 19:56:46 -0300

    Retocando el boton de start

--- simonadice/src/pages/Home.jsx
+++ simonadice/src/pages/Home.jsx
@@ -6,19 +6,11 @@ import "./styles/Home.css";
 class Home extends React.Component {
   render() {
     return (
-<<<<<<< HEAD
-      <section className="">
-        <img src={logo} alt="" />
-        <Start />
-=======
       <section className="main-container">
         <div className="logo-container">
           <img src={logo} alt="logo women who code" />
         </div>
-        <Link className="btn btn-primary" to="/game">
-            <Start />
-          </Link>
->>>>>>> 930c743df62cf92971901a742ce923540308f591
+        <Start />
       </section>
     );
   }
